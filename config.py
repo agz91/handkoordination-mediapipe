95760a6be395860ef4b02df51d0358f421fb726c
Raphael <raphimue1312@gmail.com>
2025-10-09 14:17:51 +0200

    Modularisierung finalisiert

--- config.py
+++ config.py
@@ -12,13 +12,25 @@ def init():
     cap_frame_height = 1080
 
     global FINGER_COMPARE_TOLERANCE
-    FINGER_COMPARE_TOLERANCE = 20
+    FINGER_COMPARE_TOLERANCE = 50
 
     global mp_hands
     mp_hands = mp.solutions.hands
 
     global cap
     cap = cv2.VideoCapture(0)
+
+    global INDEX_FINGER_TIP_compare1_X
+    INDEX_FINGER_TIP_compare1_X = 1
+
+    global INDEX_FINGER_TIP_compare2_X
+    INDEX_FINGER_TIP_compare2_X = 1
+
+    global INDEX_FINGER_TIP_compare1_Y
+    INDEX_FINGER_TIP_compare1_Y = 1
+
+    global INDEX_FINGER_TIP_compare2_Y
+    INDEX_FINGER_TIP_compare2_Y = 1
 
 
 
--- gesture_logic.py
+++ gesture_logic.py
@@ -2,30 +2,21 @@ import config
 
 print("Importing gesture_logic module...\n")
 
-counter = 0
-
-global INDEX_FINGER_TIP_compare1_X
-INDEX_FINGER_TIP_compare1_X = 0
-
-global INDEX_FINGER_TIP_compare2_X
-INDEX_FINGER_TIP_compare2_X = 0
-
-global INDEX_FINGER_TIP_compare1_Y
-INDEX_FINGER_TIP_compare1_Y = 0
-
-global INDEX_FINGER_TIP_compare2_Y
-INDEX_FINGER_TIP_compare2_Y = 0
+counter = 0 
 
 def INDEX_FINGER_TIP_TOUCH(Handdetection_results):
+  counter = 0
   if Handdetection_results.multi_hand_landmarks:
     for hand_landmarks in Handdetection_results.multi_hand_landmarks:
       INDEX_FINGER_TIP = hand_landmarks.landmark[config.mp_hands.HandLandmark.INDEX_FINGER_TIP]
       HANDEDNESS = Handdetection_results.multi_handedness 
       # calculate pixel on screen in relation to coordinate of hand (scale 0-1) (as whole number)
-
+      counter += 1
       coord_INDEX_FINGER_TIP_X = int((1 - INDEX_FINGER_TIP.x) * config.cap_frame_width)
       coord_INDEX_FINGER_TIP_Y = int(INDEX_FINGER_TIP.y * config.cap_frame_height)
-
+      print(counter)
+      print(coord_INDEX_FINGER_TIP_X)
+      print(coord_INDEX_FINGER_TIP_Y)
 
 
       HANDEDNESS = str(HANDEDNESS)
@@ -43,27 +34,30 @@ def INDEX_FINGER_TIP_TOUCH(Handdetection_results):
         state = 0
 
 
-      if counter % 2 == 0:
+      if counter % 2 == 1:
         config.INDEX_FINGER_TIP_compare1_X = coord_INDEX_FINGER_TIP_X
         config.INDEX_FINGER_TIP_compare1_Y = coord_INDEX_FINGER_TIP_Y
-      elif counter % 2 == 1:
+      elif counter % 2 == 0:
         config.INDEX_FINGER_TIP_compare2_X = coord_INDEX_FINGER_TIP_X
         config.INDEX_FINGER_TIP_compare2_Y = coord_INDEX_FINGER_TIP_Y
 
-      if((abs(INDEX_FINGER_TIP_compare1_X-INDEX_FINGER_TIP_compare2_X)) <= config.FINGER_COMPARE_TOLERANCE and state == 3):
+
+      if((abs(config.INDEX_FINGER_TIP_compare1_X-config.INDEX_FINGER_TIP_compare2_X)) <= config.FINGER_COMPARE_TOLERANCE and state == 3):
         statex = True
       else:
         statex = False
 
-      if((abs(INDEX_FINGER_TIP_compare1_Y-INDEX_FINGER_TIP_compare2_Y)) <= config.FINGER_COMPARE_TOLERANCE and state == 3):
+      if((abs(config.INDEX_FINGER_TIP_compare1_Y-config.INDEX_FINGER_TIP_compare2_Y)) <= config.FINGER_COMPARE_TOLERANCE and state == 3):
         statey = True
       else:
         statey = False
+      print("State X: ",config.INDEX_FINGER_TIP_compare1_X," - ",config.INDEX_FINGER_TIP_compare2_X," = ",statex)
+      print("State Y: ",config.INDEX_FINGER_TIP_compare1_Y," - ",config.INDEX_FINGER_TIP_compare2_Y," = ",statey)
+      if (statex and statey and counter == 2):
+        return True
+      elif (counter == 2):
+        return False
 
-      if (statex and statey):
-        return True           
-      #print("State X: ",INDEX_FINGER_TIP_compare1_X," - ",INDEX_FINGER_TIP_compare2_X," = ",statex)
-      #print("State Y: ",INDEX_FINGER_TIP_compare1_Y," - ",INDEX_FINGER_TIP_compare2_Y," = ",statey)
 
 
 if __name__ == '__main__':
