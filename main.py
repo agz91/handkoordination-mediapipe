9ca71cb488e05469442c81f76aff04c2a9dcbfda
Raphael Stopa <stopa.raphael@gmail.com>
2026-02-06 19:24:23 +0100

    kommentare main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+# ----- import der benötigten biblotheken und module -----
+# konfiguratiuonsmodul mit global verwendeten variablen
 import config
+# macht ein bild und gibt dieses an das machine learning modell
+# weiter, gibt eine liste mit resultaten aus
 import handdetection
+# gibt die zu benutzende kamera aufgrund einer gestik id aus
 from camera_id_extractor import get_camera_id
+# durchsucht die angeschlossenen kameras und ordnet sie richtig
+# zu
 import cam_selector
+# bestimmt aus der resultat liste ob eine übung richtig gemacht
+# wurde
 from gesture_logic_v2 import gesture_logic
+# bibliothek für die verwendung der kameras
 import cv2
+# eingebaute bibliothek mit verschiedenen funktionen zur zeit
 import time
+# initialisiert die variablen der konfigurationsdatei
 config.init()
 
+# ----- inhalt des hauptprogrammes -----
 while True:
+
+	# testet die resultate des cam_selectors
 	#image = cam_selector.find_camera()
 	#for i in range(len(image)):
 	#	if image[i] is not None:
@@ -15,15 +30,8 @@ while True:
 	#		cv2.waitKey(0)
 	#		cv2.destroyAllWindows()
 
-	#gesture_id = 10
-	#print(gesture_logic(gesture_id, handdetection.detectHand(get_camera_id(gesture_id))))
-	#extractor.Extractor(handdetection.detectHand())
-	#print(index_finger_tip_touch.INDEX_FINGER_TIP_TOUCH(handdetection.detectHand()))
-	#print(index_thumb_tip_touch.INDEX_THUMB_TIP_TOUCH(handdetection.detectHand()))
-	#print(thumb_middle_tip_touch.THUMB_MIDDLE_TIP_TOUCH(handdetection.detectHand()))
-	#print(thumb_ring_tip_touch.THUMB_RING_TIP_TOUCH(handdetection.detectHand()))
-	#print(thumb_pinky_tip_touch.THUMB_PINKY_TIP_TOUCH(handdetection.detectHand())) 
+	# gestik id für die zu testende gestik
+	gesture_id = 10
+	# druckt die resultate der gesten logik der mitgegebenen gestik in die konsole
+	print(gesture_logic(gesture_id, handdetection.detectHand(get_camera_id(gesture_id))))
 	time.sleep(1)
-
-cv2.destroyAllWindows()
-cv2.VideoCapturerelease()
